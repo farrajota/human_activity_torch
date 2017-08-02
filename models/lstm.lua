b9bfaa3ddb93d73c0d8cb9d8f6f2a636c00bad43
farrajota <mglfarrajota@outlook.com>
2017-08-02 15:37:58 +0100

    Add lstm network classifier

--- models/lstm.lua
+++ models/lstm.lua
@@ -2,4 +2,23 @@
     LSTM network to classify a sequence of images.
 ]]
 
--- TODO
+
+require 'nn'
+require 'cudnn'
+
+------------------------------------------------------------------------------------------------------------
+
+local function load_network(input_size, num_feats, num_activities, num_layers, batchSize, seq_length)
+    local view = nn.View(batchSize * seq_length, -1)
+    local lstm = nn.Sequential()
+    lstm:add(nn.Contiguous())
+    lstm:add(cudnn.LSTM(input_size, num_feats, num_layers, true))
+    lstm:add(nn.Contiguous())
+    lstm:add(view)
+    lstm:add(nn.Linear(num_feats, num_activities))
+    return lstm, view
+end
+
+------------------------------------------------------------------------------------------------------------
+
+return load_network
