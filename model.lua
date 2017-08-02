9b0ddf111757ade53978ef0f4a305d346b74094d
farrajota <mglfarrajota@outlook.com>
2017-08-02 14:27:38 +0100

    Remove cpu branch

--- model.lua
+++ model.lua
@@ -49,55 +49,32 @@ local criterion = nn.CrossEntropyCriterion()
 -- Convert to GPU or CPU
 --------------------------------------------------------------------------------
 
-if opt.GPU >= 1 then
-    print('Running on GPU: [' .. opt.nGPU .. ']')
-    require 'cutorch'
-    require 'cunn'
-    model:cuda()
-    criterion:cuda()
-
-   -- require cudnn if available
-    if pcall(require, 'cudnn') then
-        cudnn.convert(model, cudnn):cuda()
-        cudnn.benchmark = true
-        if opt.cudnn_deterministic then
-            model:apply(function(m) if m.setMode then m:setMode(1,1,1) end end)
-        end
-        print('Network has', #model:findModules'cudnn.SpatialConvolution', 'cudnn convolutions')
-    end
-    opt.dataType = 'torch.CudaTensor'
-else
-    print('Running on CPU')
-    model:float()
-    criterion:float()
-    opt.dataType = 'torch.FloatTensor'
-end
+print('Running on GPU: [' .. opt.nGPU .. ']')
+model:cuda()
+criterion:cuda()
+opt.dataType = 'torch.CudaTensor'
 
 
 --------------------------------------------------------------------------------
 -- Config network to use multiple GPUs
 --------------------------------------------------------------------------------
 
-local modelOut = nn.Sequential()
-
 -- Use multiple gpus
 if opt.GPU >= 1 and opt.nGPU > 1 then
     if torch.type(model) == 'nn.DataParallelTable' then
-        modelOut:add(utils.loadDataParallel(model, opt.nGPU))
+        model.features = utils.loadDataParallel(model, opt.nGPU)
     else
-        modelOut:add(utils.makeDataParallelTable(model, opt.nGPU))
+        model.features = utils.makeDataParallelTable(model, opt.nGPU)
     end
-else
-    modelOut:add(model)
 end
 
 local function cast(x) return x:type(opt.data_type) end
 
-cast(modelOut)
+cast(model)
 
 
 --------------------------------------------------------------------------------
 -- Output
 --------------------------------------------------------------------------------
 
-return modelOut, criterion
+return model, criterion
