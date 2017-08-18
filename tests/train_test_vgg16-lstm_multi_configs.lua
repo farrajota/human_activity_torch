5ef9d99cae5c9f02e148de12138c16fd11da1ff8
farrajota <mglfarrajota@outlook.com>
2017-08-18 10:35:42 +0100

    Add logging to the hyperparameter test script

--- tests/train_test_vgg16-lstm_multi_configs.lua
+++ tests/train_test_vgg16-lstm_multi_configs.lua
@@ -2,11 +2,50 @@
     Train and test a LSTM network to classify a sequence of images.
 ]]
 
+
+require 'optim'
+local Logger = optim.Logger
+paths.dofile('../projectdir.lua')
+
+local logger_filename = paths.concat(projectDir,'exp', 'ucf_sports', 'test_hyperparameters_vgg16-lstm.log')
+if not paths.dirp(paths.dirname(logger_filename)) then
+    print('Creating dir: ' .. paths.dirname(logger_filename))
+    os.execute('mkdir -p ' .. paths.dirname(logger_filename))
+end
+logger = optim.Logger(logger_filename, false)
+logger:setNames{'ExperimentID','Top-1 accuracy (%)','Top-5 accuracy (%)', 'Average Precision'}
+
+------------------------------------------------------------------------------------------------------------
+
 local function exec_command(command)
     print('\n')
     print('Executing command: ' .. command)
     print('\n')
     os.execute(command)
+end
+
+------------------------------------------------------------------------------------------------------------
+
+local function read_log_file(opt)
+    local filename = paths.concat(projectDir, 'exp', opt.dataset, opt.expID, 'Evaluation_full.log')
+    if paths.filep(filename) then
+        local file = io.open(filename, 'r')
+        local stats
+        for line in file:lines() do
+            stats = string.split(line, '\t')
+        end
+        file:close()
+        return stats
+    end
+    return nil
+end
+
+------------------------------------------------------------------------------------------------------------
+
+local function log(opt)
+    -- get accuracy from the network
+    local stats = read_log_file(opt)
+    logger:add{opt.expID, tonumber(stats[1]), tonumber(stats[2]), tonumber(stats[3])}
 end
 
 ------------------------------------------------------------------------------------------------------------
@@ -63,9 +102,9 @@ local test_opts = {
     {expID = 'vgg16-lstm-test7', seq_length = 10, batchSize = 16},
     {expID = 'vgg16-lstm-test8', seq_length = 20, batchSize = 16},
     {expID = 'vgg16-lstm-test9', seq_length = 30, batchSize = 16},
-    {expID = 'vgg16-lstm-test10', seq_length = 10, batchSize = 32},
-    {expID = 'vgg16-lstm-test11', seq_length = 20, batchSize = 32},
-    {expID = 'vgg16-lstm-test12', seq_length = 30, batchSize = 32},
+    --{expID = 'vgg16-lstm-test10', seq_length = 10, batchSize = 32},
+    --{expID = 'vgg16-lstm-test11', seq_length = 20, batchSize = 32},
+    --{expID = 'vgg16-lstm-test12', seq_length = 30, batchSize = 32},
 
     -- try different nFeats and nLayers
     {expID = 'vgg16-lstm-test13', seq_length = 10, batchSize = 4, nFeats = 256, nLayers = 1,},
@@ -92,9 +131,10 @@ local test_opts = {
     {expID = 'vgg16-lstm-test30', seq_length = 10, batchSize = 4, rotRate = 0,  scale = 0,   rotate = 0,},
 }
 
-
 for i, test_opt in ipairs(test_opts) do
+    print('\n=======================================================')
     print(('Starting train+test configuration: %d/%d'):format(i, #test_opts))
+    print('=======================================================\n')
 
     -- set train + test options
     local opts = get_configs()
@@ -113,5 +153,7 @@ for i, test_opt in ipairs(test_opts) do
 
     -- test network
     exec_command(('th test.lua %s'):format(str_args))
+
+    log(opts)
 end
 
