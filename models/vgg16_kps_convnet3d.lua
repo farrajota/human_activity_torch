966eee6a46d54694f3afbf20ce6bfb8217f15271
farrajota <mglfarrajota@outlook.com>
2017-08-18 12:10:59 +0100

    Add code for vgg16+kps model

--- models/vgg16_kps_convnet3d.lua
+++ models/vgg16_kps_convnet3d.lua
@@ -1,3 +1,28 @@
 --[[
     Load VGG16 + Body joints predictor + Temporal ConvNet networks.
 ]]
+
+
+------------------------------------------------------------------------------------------------------------
+
+--[[ Create VGG16 + LSTM ]]--
+local function create_network()
+
+    local vgg16 = paths.dofile('vgg16_convnet3d.lua')
+    local kps = paths.dofile('kps_convnet3d.lua')
+
+    local vgg16_features, _, vgg16_convnet, vgg16_params = vgg16()
+    local _, kps_features, kps_convnet, _ = kps()
+
+    local classifier = nn.Sequential()
+    classifier:add(nn.ConcatTable()
+        :add(nn.Sequential():add(nn.SelectTable(1)):add(vgg16_convnet))
+        :add(nn.Sequential():add(nn.SelectTable(2)):add(kps_convnet)))
+    classifier:add(nn.CAddTable())
+
+    return vgg16_features, kps_features, classifier, vgg16_params  -- features, kps, classifier, params
+end
+
+------------------------------------------------------------------------------------------------------------
+
+return create_network
