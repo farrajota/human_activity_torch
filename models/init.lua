165a1418b507af33d58efdd0a8af364e53129b43
farrajota <mglfarrajota@outlook.com>
2017-08-18 12:11:48 +0100

    Add models to list

--- models/init.lua
+++ models/init.lua
@@ -16,6 +16,10 @@ network_list['vgg16-kps-lstm'] = paths.dofile('vgg16_kps_lstm.lua')
 network_list['vgg16-convnet3d'] = paths.dofile('vgg16_convnet3d.lua')
 network_list['vgg16-convnet3d_2'] = paths.dofile('vgg16_convnet3d_2.lua')
 network_list['kps-convnet3d'] = paths.dofile('kps_convnet3d.lua')
+network_list['kps-convnet3d_2'] = paths.dofile('kps_convnet3d_2.lua')
+network_list['kps-convnet3d_3'] = paths.dofile('kps_convnet3d_3.lua')
+network_list['kps-convnet3d_4'] = paths.dofile('kps_convnet3d_4.lua')
 network_list['vgg16-kps-convnet3d'] = paths.dofile('vgg16_kps_convnet3d.lua')
+network_list['vgg16-kps-convnet3d_2'] = paths.dofile('vgg16_kps_convnet3d_2.lua')
 
 return network_list
