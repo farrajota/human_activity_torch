44638761d80956e9d7152d45d41ec6b6abbf5739
farrajota <mglfarrajota@outlook.com>
2017-08-18 12:09:08 +0100

    Add convnet test models

---
+++ models/kps_convnet3d_2.lua
@@ -0,0 +1,63 @@
+--[[
+    Load Kps + ConvNet (avg pool + lin layer) networks.
+]]
+
+
+
+require 'nn'
+
+
+------------------------------------------------------------------------------------------------------------
+
+local function load_features_network()
+    local filepath = paths.concat(projectDir, 'data', 'pretrained_models')
+    local hg_net = torch.load(paths.concat(filepath, 'hg-best.t7'))
+    local net = nn.Sequential()
+    net:add(hg_net)
+    net:add(nn.SelectTable(-1))
+    local params = {
+      pixel_scale = 1,
+      dims = {11, 64, 64},
+      feat_size = 11
+    }
+    return net, params
+end
+
+------------------------------------------------------------------------------------------------------------
+
+local function load_classifier_network(input_size, num_feats, num_activities, num_layers, seq_length)
+    local classifier = nn.Sequential()
+    classifier:add(nn.Transpose({2,3}))  -- swap dim2 with dim4 such that input = B x input_size x seq_length x 1
+    classifier:add(nn.VolumetricAveragePooling(1, 64, 64, 1,1,1))
+    classifier:add(nn.VolumetricConvolution(input_size, num_feats, 3,1,1, 1,1,1, 1,0,0))
+    classifier:add(nn.ReLU(true))
+    for i=2, num_layers do
+        classifier:add(nn.VolumetricConvolution(num_feats, num_feats, 3,1,1, 1,1,1, 1,0,0))
+        classifier:add(nn.ReLU(true))
+    end
+    classifier:add(nn.VolumetricAveragePooling(seq_length,1,1, 1,1,1))
+    classifier:add(nn.VolumetricConvolution(num_feats, num_activities, 1,1,1, 1,1,1))
+    classifier:add(nn.View(-1, num_activities))
+    return classifier
+end
+
+------------------------------------------------------------------------------------------------------------
+
+--[[ Create VGG16 + spatial average pooling + lin layer ]]--
+local function create_network()
+
+    local kps_features, params = load_features_network()
+    kps_features:evaluate()
+
+    local classifier = load_classifier_network(params.feat_size,
+                                               opt.nFeats,
+                                               opt.num_activities,
+                                               opt.nLayers,
+                                               opt.seq_length)
+
+    return nil, kps_features, classifier, params  -- features, kps, classifier, params
+end
+
+------------------------------------------------------------------------------------------------------------
+
+return create_network
