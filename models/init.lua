d7c0d678716c6c9ce70913c79b980f4210cd0c15
farrajota <mglfarrajota@outlook.com>
2017-08-30 10:46:11 +0100

    Fix var name

--- models/init.lua
+++ models/init.lua
@@ -7,6 +7,6 @@ require 'nn'
 if not nn.NoBackprop then paths.dofile('modules/NoBackprop.lua') end
 
 local network_list = {}
-paths.dofile('test/init.lua')(model_list)
+paths.dofile('test/init.lua')(network_list)
 
 return network_list
