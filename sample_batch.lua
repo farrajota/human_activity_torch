24c2bcc8fab42177858787daa5a48de2bb27d822
farrajota <mglfarrajota@outlook.com>
2017-09-04 16:16:01 +0100

    Refactor data  sampling

--- sample_batch.lua
+++ sample_batch.lua
@@ -63,12 +63,12 @@ local function get_random_transforms(is_train)
 
         -- rotation
         rot = rnd(opt.rotate)
-        if torch.uniform() <= opt.rotRate then
+        if torch.uniform() > opt.rotRate then
             rot = 0
         end
 
         -- Flipping
-        if torch.uniform() < .5 then
+        if torch.uniform() > .5 then
             flip = true
         end
 
@@ -124,86 +124,69 @@ end
 
 ------------------------------------------------------------------------------------------------------------
 
---local function fetch_subset_images(imgs, seq_length)
---    local out_imgs = {}
---    local nimgs = #imgs
---    if nimgs > seq_length then
---        local idx_ini = math.random(1, nimgs - seq_length) - 1  -- select a subset of images from the video
---        for i=1, seq_length do
---            table.insert(out_imgs, imgs[idx_ini + i])
---        end
---    else
---        -- loop the sequence video
---        local idx_ini = math.random(1, nimgs)
---        for i=1, seq_length do
---            table.insert(out_imgs, imgs[idx_ini])
---            idx_ini = idx_ini + 1
---            if idx_ini > nimgs then
---                idx_ini = 1
---            end
---        end
---    end
---    collectgarbage()
---    return out_imgs
---end
+local function fetch_subset_images(nimgs, seq_length, step)
+    assert(nimgs)
+    assert(seq_length)
+    assert(step)
 
-local function fetch_subset_images(nimgs, seq_length)
     local out_idx = {}
-    if nimgs > seq_length then
-        -- select a subset of images from the video
-        local idx_ini = math.random(1, nimgs - seq_length) - 1
-        for i=1, seq_length do
-            table.insert(out_idx, idx_ini + i)
-        end
+
+    local idx_ini
+    if nimgs < seq_length*step then
+        idx_ini = math.random(1, nimgs)  -- start randomly (the vid will loop)
     else
-        -- loop the sequence video
-        local idx_ini = math.random(1, nimgs)
-        for i=1, seq_length do
-            table.insert(out_idx, idx_ini)
-            idx_ini = idx_ini + 1
-            if idx_ini > nimgs then
-                idx_ini = 1
-            end
-        end
+        idx_ini = math.random(1, nimgs - seq_length*step)
     end
+
+    table.insert(out_idx, idx_ini)
+    local idx_next = idx_ini
+    for i=1, seq_length-1 do
+        idx_next = idx_next + step
+        if idx_next > nimgs then
+            idx_next = idx_next - nimgs
+        end
+        table.insert(out_idx, idx_next)
+    end
+
     return out_idx
 end
 
 ------------------------------------------------------------------------------------------------------------
 
-local function process_images_train(imgs, is_train)
+local function process_images_heatmaps(imgs, idxs, params_transform, is_test)
     assert(imgs)
-    assert(is_train ~= nil)
+    assert(idxs)
+    assert(params_transform)
 
-    -- select a subset of images from the video
-    --imgs_s = fetch_subset_images(imgs, opt.seq_length)
-    local idxs = fetch_subset_images(#imgs, opt.seq_length)
-
-     -- select some random transformations to apply to the entire set of images
-    local params_transform = get_random_transforms(is_train)
+    local is_test = is_test or false
 
     local imgs_transf = {}
-    if opt.process_input_heatmap or opt.use_center_crop then
-        local prev_center, prev_scale = imgs[1].center, imgs[1].scale
-        local prev_bbox = imgs[1].bbox
-        if prev_bbox:sum() == 0 or prev_bbox[4]-prev_bbox[2] < 20 then
-            prev_bbox = get_center_crop_bbox(imgs[1].img)
-            prev_center = torch.FloatTensor({(prev_bbox[1] + prev_bbox[3])/2,
-                                            (prev_bbox[2] + prev_bbox[4])/2})
-            prev_scale = (prev_bbox[4]-prev_bbox[2]) / 200 * 1.5
+
+    local prev_center, prev_scale = imgs[1].center, imgs[1].scale
+    local prev_bbox = imgs[1].bbox
+    if prev_bbox:sum() == 0 or prev_bbox[4]-prev_bbox[2] < 20 then
+        prev_bbox = get_center_crop_bbox(imgs[1].img)
+        prev_center = torch.FloatTensor({(prev_bbox[1] + prev_bbox[3])/2,
+                                        (prev_bbox[2] + prev_bbox[4])/2})
+        prev_scale = (prev_bbox[4]-prev_bbox[2]) / 200 * 1.25
+    end
+    for _, idx in ipairs(idxs) do
+        local img = imgs[idx].img
+        local scale = imgs[idx].scale
+        local center = imgs[idx].center
+        local bbox = imgs[idx].bbox
+
+        if scale < 0 or bbox:sum() == 0 or bbox[4]-bbox[2] < 20 then
+            bbox = prev_bbox
+            center = prev_center
+            scale = prev_scale
         end
-        for _, idx in ipairs(idxs) do
-            local img = imgs[idx].img
-            local scale = imgs[idx].scale
-            local center = imgs[idx].center
-            local bbox = imgs[idx].bbox
 
-            if scale < 0 or bbox:sum() == 0 or bbox[4]-bbox[2] < 20 then
-                imgs[idx].bbox = prev_bbox
-                imgs[idx].center = prev_center
-                imgs[idx].scale = prev_scale
-            end
-
+        if is_test then
+            local img_crop = crop2(img, center, scale, 0, opt.inputRes)
+            if not img_crop then return {} end -- skip this round of data/transforms if any error occurs
+            table.insert(imgs_transf, img_crop)
+        else
             if not opt.same_transform then
                 params_transform = get_random_transforms(is_train)
             end
@@ -211,22 +194,38 @@ local function process_images_train(imgs, is_train)
             local new_img = transform_data(img, center, scale, params_transform)
             if not new_img then return {} end  -- skip this round of data/transforms if any error occurs
             table.insert(imgs_transf, new_img)
+        end
 
-            if not (scale < 0 or bbox:sum() == 0 or bbox[4]-bbox[2] < 20) then
-                prev_bbox = bbox
-                prev_center = center
-                prev_scale = scale
-            end
+        if not (scale < 0 or bbox:sum() == 0 or bbox[4]-bbox[2] < 20) then
+            prev_bbox = bbox
+            prev_center = center
+            prev_scale = scale
         end
     end
 
-    -- images for global feature extractiong
+    return imgs_transf
+end
+
+------------------------------------------------------------------------------------------------------------
+
+local function process_images_crops(imgs, imgs_transf, idxs, params_transform, is_test)
+    assert(imgs)
+    assert(idxs)
+    assert(params_transform)
+
+    local is_test = is_test or false
+
     local imgs_resized = {}
-    if opt.process_input_feats then
-        if opt.use_center_crop then
-            local iW, iH
-            local img_size = torch.random(224,256)
-            for i=1, #imgs_transf do
+
+    if opt.use_center_crop then
+        local iW, iH
+        local img_size = torch.random(224,256)
+        for i=1, #imgs_transf do
+            if is_test then
+                local img = imgs_transf[i]
+                local new_img = image.scale(img, 224)
+                table.insert(imgs_resized, normalize_image(new_img))
+            else
                 if not opt.same_transform then
                     img_size = torch.random(224,256)
                 end
@@ -239,89 +238,33 @@ local function process_images_train(imgs, is_train)
                 new_img = new_img[{{}, {iH, iH+224 -1}, {iW, iW + 224 -1}}]
                 table.insert(imgs_resized, normalize_image(new_img))
             end
-        else
-            local iW, iH
-            local img_size = torch.random(224,256)
-            for _, idx in ipairs(idxs) do
-                if  not opt.same_transform then
+        end
+    else
+        local iW, iH
+        local img_size = torch.random(224,256)
+        for _, idx in ipairs(idxs) do
+            local img = imgs[idx].img
+            if is_test then
+                new_img = resize_image(img, 224)
+                iW = math.max(1, math.floor((new_img:size(3)-224)/2))
+                iH = math.max(1, math.floor((new_img:size(2)-224)/2))
+            else
+                if not opt.same_transform or true then  -- added or true for testing
                     img_size = torch.random(224,256)
                 end
-
-                local new_img = resize_image(imgs[idx].img, img_size)
-                if (iW == nil or iH == nil) or not opt.same_transform then
+                new_img = resize_image(img, img_size)
+                if (iW == nil or iH == nil) or not opt.same_transform or true then  -- added or true for testing
                     iW = torch.random(1, math.max(1, new_img:size(3) - 224))
                     iH = torch.random(1, math.max(1, new_img:size(2) - 224))
                 end
-                new_img = new_img[{{}, {iH, iH+224 -1}, {iW, iW + 224 -1}}]
-                table.insert(imgs_resized, normalize_image(new_img))
             end
+
+            new_img = new_img[{{}, {iH, iH+224 -1}, {iW, iW + 224 -1}}]
+            table.insert(imgs_resized, normalize_image(new_img))
         end
     end
 
-    return imgs_transf, imgs_resized
-end
-
-------------------------------------------------------------------------------------------------------------
-
-local function process_images_test(imgs)
-    assert(imgs)
-
-    local imgs_transf = {}
-    if opt.process_input_heatmap or opt.use_center_crop then
-        local prev_center, prev_scale = imgs[1].center, imgs[1].scale
-        local prev_bbox = imgs[1].bbox
-        if prev_bbox:sum() == 0 or prev_bbox[4]-prev_bbox[2] < 20 then
-            prev_bbox = get_center_crop_bbox(imgs[1].img)
-            prev_center = torch.FloatTensor({(prev_bbox[1] + prev_bbox[3])/2,
-                                            (prev_bbox[2] + prev_bbox[4])/2})
-            prev_scale = (prev_bbox[4]-prev_bbox[2]) / 200 * 1.5
-        end
-        for i=1, #imgs do
-            local img = imgs[i].img:clone()
-            local scale = imgs[i].scale
-            local center = imgs[i].center
-            local bbox = imgs[i].bbox
-
-            if scale < 0 or bbox:sum() == 0 or bbox[4]-bbox[2] < 20 then
-                imgs[i].bbox = prev_bbox
-                imgs[i].center = prev_center
-                imgs[i].scale = prev_scale
-            end
-
-            local img_crop = crop2(img, center, 1, 0, opt.inputRes)
-            if not img_crop then return {} end -- skip this round of data/transforms if any error occurs
-
-            table.insert(imgs_transf, img_crop)
-
-            if not (scale < 0 or bbox:sum() == 0 or bbox[4]-bbox[2] < 20) then
-                prev_bbox = bbox
-                prev_center = center
-                prev_scale = scale
-            end
-        end
-    end
-
-    -- images for global feature extractiong
-    local imgs_resized = {}
-    if opt.process_input_feats then
-        if opt.use_center_crop then
-            for i=1, #imgs_transf do
-                local img = imgs_transf[i]
-                local new_img = image.scale(img, 224)
-                table.insert(imgs_resized, normalize_image(new_img))
-            end
-        else
-            for i=1, #imgs do
-                local new_img = resize_image(imgs[i].img, 224)
-                local iW = math.max(1, math.floor((new_img:size(3)-224)/2))
-                local iH = math.max(1, math.floor((new_img:size(2)-224)/2))
-                new_img = new_img[{{}, {iH, iH+224 -1}, {iW, iW + 224 -1}}]
-                table.insert(imgs_resized, normalize_image(new_img))
-            end
-        end
-    end
-
-    return imgs_transf, imgs_resized
+    return imgs_resized
 end
 
 ------------------------------------------------------------------------------------------------------------
@@ -338,11 +281,36 @@ local function fetch_single_data(data_loader, idx, is_train, use_subset)
     local imgs, label = data_loader.loader(idx)
 
     -- get cropped images + heatmaps
-    local imgs_transf, imgs_resized
+    local imgs_transf, imgs_resized = {}, {}
     if is_train or use_subset then
-        imgs_transf, imgs_resized = process_images_train(imgs, is_train)
+        -- select a subset of images from the video
+        local idxs = fetch_subset_images(#imgs, opt.seq_length, opt.step)
+
+        -- select some random transformations to apply to the entire set of images
+        local params_transform = get_random_transforms(is_train)
+
+        -- process heatmaps
+        if opt.process_input_heatmap or opt.use_center_crop then
+            imgs_transf = process_images_heatmaps(imgs, idxs, params_transform, false)
+        end
+
+        -- process images
+        if opt.process_input_feats then
+            imgs_resized = process_images_crops(imgs, imgs_transf, idxs, params_transform, false)
+        end
     else
-        imgs_transf, imgs_resized = process_images_test(imgs)
+        -- select a subset of images from the video
+        local idxs = fetch_subset_images(#imgs, opt.test_seq_length, opt.test_step)
+
+        -- process heatmaps
+        if opt.process_input_heatmap or opt.use_center_crop then
+            imgs_transf = process_images_heatmaps(imgs, idxs, {}, true)
+        end
+
+        -- process images
+        if opt.process_input_feats then
+            imgs_resized = process_images_crops(imgs, imgs_transf, idxs, {}, true)
+        end
     end
 
     imgs = nil
@@ -361,16 +329,16 @@ local function get_batch(data_loader, batchSize, is_train)
     assert(type(is_train) == 'boolean')
 
     local size = data_loader.num_activities
-    local max_attempts = 30
+    local max_attempts = 10
     local batchData, idxUsed = {}, {}
 
     for i=1, batchSize do
         local data = {}
         local attempts = 0
         -- select a random activity
-        local activity_id = math.random(1, data_loader.num_activities)
         while not next(data) do
 
+            local activity_id = math.random(1, data_loader.num_activities)
             -- select a random video from the selected activity
             local video_ids = data_loader.get_video_ids(activity_id)
             local video_id = video_ids[math.random(1, #video_ids)] + 1  -- set to 1-index
@@ -468,8 +436,9 @@ function getSampleTest(data_loader, idx)
     -- get batch data
     local sample = fetch_single_data(data_loader, idx, false, false)
 
+    --[[
     -- sample size
-    local sample_seq_length = #sample[1]
+    local sample_seq_length = math.max(#sample[1], #sample[2])
     local min_seq_length = opt.seq_length
     local seq_length = math.max(min_seq_length, sample_seq_length)
     local ini = 0  -- this padding is needed for cases where the test sequence length is smaller than the train seq length
@@ -502,6 +471,16 @@ function getSampleTest(data_loader, idx)
         if ini_id > sample_seq_length then
             ini_id = 1
         end
+    end
+    ]]
+
+    local seq_length = math.max(#sample[1], #sample[2])
+    local imgs_hms, imgs_feats
+    if next(sample[1]) then imgs_hms = torch.FloatTensor(1, seq_length, 3, opt.inputRes, opt.inputRes):fill(0) end
+    if next(sample[2]) then imgs_feats = torch.FloatTensor(1, seq_length, 3, 224, 224):fill(0) end
+    for i=1, seq_length do
+        if imgs_hms then imgs_hms[1][i]:copy(sample[1][i]) end
+        if imgs_feats then imgs_feats[1][i]:copy(sample[2][i]) end
     end
 
     -- labels
