cc71ed300ea6cce1da8edd72eee2655df9968882
farrajota <mglfarrajota@outlook.com>
2017-09-27 15:06:15 +0100

    Fix network id

--- models/init.lua
+++ models/init.lua
@@ -11,6 +11,6 @@ paths.dofile('test/init.lua')(network_list)
 
 network_list['resnet50-lstm'] = paths.dofile('test/resnet50_lstm.lua')
 network_list['hms-lstm'] = paths.dofile('test/hms_lstm.lua')
-network_list['vgg16-hms-lstm'] = paths.dofile('test/vgg16_hms_lstm_v2.lua')
+network_list['resnet50-hms-lstm'] = paths.dofile('test/resnet50_hms_lstm_v2.lua')
 
 return network_list
