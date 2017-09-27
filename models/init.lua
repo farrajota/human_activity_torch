903eb3b47a02c579c1cb047e9a86f04526d06c2f
farrajota <mglfarrajota@outlook.com>
2017-09-27 15:01:49 +0100

    Add final models for train + test

--- models/init.lua
+++ models/init.lua
@@ -9,4 +9,8 @@ if not nn.NoBackprop then paths.dofile('modules/NoBackprop.lua') end
 local network_list = {}
 paths.dofile('test/init.lua')(network_list)
 
+network_list['resnet50-lstm'] = paths.dofile('test/resnet50_lstm.lua')
+network_list['hms-lstm'] = paths.dofile('test/hms_lstm.lua')
+network_list['vgg16-hms-lstm'] = paths.dofile('test/vgg16_hms_lstm_v2.lua')
+
 return network_list
